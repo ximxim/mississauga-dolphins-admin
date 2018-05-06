cfe48d25bf4198760fc6b4ed339dd9757fb95ece
Azim Ahmed <azim.ahmed7@gmail.com>
2018-05-06 18:12:08 -0400

    added react-router to the project

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Redirect, Route, BrowserRouter } from 'react-router-dom';
+
 import withSetup from './js/redux/setup';
 
+import signin from './js/routes/signin';
+
+const InitialPath = ({ component: Component, ...rest, authUser }) =>
+    <Route
+        {...rest}
+        render={props =>
+            authUser
+                ? <Component {...props} />
+                : <Redirect
+                    to={{
+                        pathname: '/signin',
+                        state: { from: props.location }
+                    }}
+                />}
+    />;
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+    render() {
+        return (
+            <BrowserRouter>
+                <React.Fragment>
+                    <InitialPath path="/scores" authUser={this.props.user} component={signin} />
+                    <Route path="/signin" component={signin} />
+                </React.Fragment>
+            </BrowserRouter>
+        );
+    }
 }
 
 export default withSetup(App);
