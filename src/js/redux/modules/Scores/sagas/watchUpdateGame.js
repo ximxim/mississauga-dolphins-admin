1d450b28399bc7f1b6a57a88729efb789e0c24be
Azim Ahmed <azim.ahmed7@gmail.com>
2018-05-10 18:50:02 -0400

    added updated_at key to game object

--- src/js/redux/modules/Scores/sagas/watchUpdateGame.js
+++ src/js/redux/modules/Scores/sagas/watchUpdateGame.js
@@ -1,5 +1,6 @@
 import { call, takeEvery, put } from 'redux-saga/effects';
 import { toast } from 'react-toastify';
+import moment from 'moment';
 
 import { getClient } from '../../../../utils/firebase';
 import * as scoresModule from '../index';
@@ -7,6 +8,7 @@ import * as scoresModule from '../index';
 function createGame({ game, id }) {
     const fClient = getClient();
     const ref = fClient.database().ref(`/Games/${id}`);
+    game.updated_at = moment().format();
     return ref.set(game);
 }
 
